9f8db176d0cf2780aa95cbdb3d50973b075e646e
Moorits Mihkel Muru <mooritsmihkel@gmail.com>
2022-04-19 00:16:34 +0300

    lab10: jl preamble

---
+++ lab10/lab10.jl
@@ -0,0 +1,20 @@
+# Moorits Mihkel Muru, University of Tartu 2022
+# Lab 10 of the Statistical Data Science (LTMS.00.025) course
+# Solutions in Julia
+
+using DataFrames      # Tables for data
+using StatsBase       # Basic statistics tools
+using FreqTables      # Frequency tables
+using GLM             # General linear models
+using MultivariateStats # For PCA
+using StatsPlots      # Plotting
+using HTTP            # Stream data from web
+using FileIO          # Reading in a file from a web stream
+using CodecZlib       # Unpacking RData file
+using RData           # Reading RData files
+
+# Reading the data from web
+dataset_url = "http://www.ms.ut.ee/mart/andmeteadus/WB.RData"
+request = HTTP.get(dataset_url)
+buffer = CodecZlib.GzipDecompressorStream(IOBuffer(request.body))
+data = only(values(DataFrame(RData.load(Stream{format"RData"}(buffer))))) # FIXME
